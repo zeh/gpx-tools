b1aaaa4c6df3c335048035262748aa051f90d45b
Zeh Fernando <zeh@zehfernando.com>
2024-12-10 17:40:16 -0500

    Parse GPX files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::io::BufReader;
+use std::fs::File;
+
 use structopt::StructOpt;
-
 use gpx_tools::utils::files::get_files_from_masks;
-
+use gpx::read;
+use gpx::{Gpx, Track, TrackSegment};
 mod gpx_tools;
 
 #[derive(Debug, StructOpt)]
@@ -16,5 +19,47 @@ fn main() {
 	let args = Opt::from_args();
 	println!("Input: {:?}", args.input);
 	let files = get_files_from_masks(&args.input).unwrap();
-	println!("Files ({:?}): {:?}", files.len(), files);
+	println!("Files: ({:?})", files.len());
+
+	for file in files {
+		println!("  File: {:?}", file);
+		let data = File::open(file).unwrap();
+		let reader = BufReader::new(data);
+
+		let gpx: Gpx = read(reader).unwrap();
+
+		println!("    Creator: {:?}", gpx.creator.unwrap_or("<None>".to_string()));
+		println!("    Version: {:?}", gpx.version);
+
+		println!("    Tracks: {:?}", gpx.tracks.len());
+
+		for track in gpx.tracks {
+			println!("      Track: {:?}", track.name);
+			println!("        Description: {:?}", track.description);
+			println!("        Source: {:?}", track.source);
+			println!("        Segments: {:?}", track.segments.len());
+
+			for segment in track.segments {
+				println!("          Points: {:?}", segment.points.len());
+			}
+		}
+
+		println!("    Routes: {:?}", gpx.routes.len());
+
+		for route in gpx.routes {
+			println!("      Route: {:?}", route.name);
+			println!("        Description: {:?}", route.description);
+			println!("        Source: {:?}", route.source);
+			println!("        Points: {:?}", route.points.len());
+		}
+
+
+		// Each GPX file has multiple "tracks", this takes the first one.
+		// let track: &Track = &gpx.tracks[0];
+		// assert_eq!(track.name, Some(String::from("Example GPX Document")));
+
+		// Each track will have different segments full of waypoints, where a
+		// waypoint contains info like latitude, longitude, and elevation.
+		// let segment: &TrackSegment = &track.segments[0];
+	}
 }
