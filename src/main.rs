d160fbcbc1dbe5603fd9b7f326f9421194d5faeb
Zeh Fernando <zeh@zehfernando.com>
2024-12-10 14:49:00 -0500

    Started creating CLI
    Following instructions from https://rust-cli.github.io/book/tutorial/setup.html somewhat, but for now using StructOpt.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "default", about = "The default use of GPX Tools")]
+struct Opt {
+	/// The input file paths or file paths mask.
+	#[structopt(short, long, parse(from_os_str))]
+	input: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Opt::from_args();
+    println!("Input: {:?}", args.input)
 }
